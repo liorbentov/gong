7a720039f4fb2d65522cd18501ccefb18a76cd6b
Lior Shub <liorbentov@gmail.com>
2017-07-11 07:28:06 +0300

    Sales tooltip

--- src/components/top-paying-customers.js
+++ src/components/top-paying-customers.js
@@ -9,54 +9,110 @@ var colorLegend = [
 ];
 
 var tooltipProps = [{
-    css: 'value',
-    prop: 'value'
+    css: 'name',
+    prop: '_id'
 }, {
-    css: 'change',
-    prop: 'colorValue',
-    display: 'Change'
+    css: 'value',
+    prop: 'value',
+    display: 'Amount',
+}, {
+    css: 'package',
+    prop: 'package',
+    display: 'Package'
+}, {
+    css: 'tests',
+    prop: 'tests',
+    display: 'No. of Tests'
+}, {
+    css: 'users',
+    prop: 'users',
+    display: 'Concurrent Users'
+}, {
+    css: 'industry',
+    prop: 'industry',
+    display: 'Industry'
 }];
 
 const data = [{
     _id: "Netflix",
-    value: 250,
+    value: 350,
     colorValue: 1,
-    selected: false
+    color: "#da1725",
+    selected: false,
+    package: 'Unlimited',
+    tests: 5,
+    users: '12K',
+    industry: 'TV'
 }, {
     _id: "Uber",
-    value: 200,
+    value: 300,
+    color: "#1d1d1d",
     colorValue: 2,
-    selected: false
+        selected: false,
+    package: 'Unlimited',
+    tests: 5,
+    users: '12K',
+    industry: 'TV'
 }, {
     _id: "Chase",
-    value: 190,
+    value: 290,
+    color: "#235cd6",
     colorValue: 3,
-    selected: false
+        selected: false,
+    package: 'Unlimited',
+    tests: 5,
+    users: '12K',
+    industry: 'TV'
 }, {
     _id: "Starbucks",
-    value: 180,
+    value: 380,
+    color: "#13d883",
     colorValue: 4,
-    selected: false
+        selected: false,
+    package: 'Unlimited',
+    tests: 5,
+    users: '12K',
+    industry: 'TV'
 }, {
     _id: "Apple",
-    value: 120,
+    value: 220,
+    color: "#505050",
     colorValue: 1,
-    selected: false
+        selected: false,
+    package: 'Unlimited',
+    tests: 5,
+    users: '12K',
+    industry: 'TV'
 }, {
     _id: "Wolksvagen",
-    value: 110,
+    value: 210,
+    color: "#4368e1",
     colorValue: 1,
-    selected: false
+        selected: false,
+    package: 'Unlimited',
+    tests: 5,
+    users: '12K',
+    industry: 'TV'
 }, {
     _id: "Nike",
-    value: 100,
+    value: 200,
+    color: "#fdbb30",
     colorValue: 1,
-    selected: false
+        selected: false,
+    package: 'Unlimited',
+    tests: 5,
+    users: '12K',
+    industry: 'TV'
 }, {
     _id: "PS",
-    value: 80,
+    value: 180,
+    color: "#235cd6",
     colorValue: 2,
-    selected: false
+        selected: false,
+    package: 'Unlimited',
+    tests: 5,
+    users: '12K',
+    industry: 'TV'
 }];
 
 class BubbleChart extends React.Component {
